9b55afa69b4c7985c9e48acba712aa78cfdb744e
yuanqingzhu <yuanqingzhu@tencent.com>
2019-04-21 19:39:47 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+
+    return "index page"
+
+
+@app.route('/hello_world')
 def hello_world():
     return 'Hello World!'
+
+
 
 
 if __name__ == '__main__':
